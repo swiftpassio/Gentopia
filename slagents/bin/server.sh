512fd5c740ecf0bed13acb1055634d28102a5407
Nagesh Bhad <nagesh@swiftlane.com>
2024-02-29 15:16:49 +0530

    first commit

--- slagents/bin/server.sh
+++ slagents/bin/server.sh
@@ -4,4 +4,4 @@ set -e
 
 
 # Start Gunicorn
-gunicorn --timeout=0 --keep-alive=30 --pythonpath=/code/Gentopia --worker-class=gevent -w=3 --worker-connections=300  --bind=0.0.0.0:5000 --log-level=info slagents.wsgi:app
+gunicorn --timeout=0 --keep-alive=30 --pythonpath=/code/Gentopia --worker-class=gevent -w=3 --worker-connections=100  --bind=0.0.0.0:8080 --log-level=info slagents.wsgi:app
